641c4968466e09f17080921dcf9dc6d820f65767
CyberLynx1021 <abdulazizali234@gmail.com>
2023-06-13 01:35:13 -0500

    React project 20 min in

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Todo from './components/Todo.jsx'
+import Title from './components/title.jsx'
+function App() {
+  return (
+    <div>
+<Title />    
+<Todo />
+     
+    </div>
+  );
+}
+
+export default App;
