f21b51643f9312d4ad35361fe61a875668f973fe
CyberLynx1021 <abdulazizali234@gmail.com>
2023-06-17 20:19:39 -0500

    React project on hold

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ useState(false)
         <input type="text" onChange={(event) => {
           console.log(event.target.value)
         }} />
-        <button>Add to do</button>
+        <button>Add todo</button>
       </div>
       <div className="todo__wrapper">
         <Todo title ="Finish Frontend Simplfied"/>
