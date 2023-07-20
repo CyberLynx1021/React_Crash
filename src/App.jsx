350b561d7fbfdad599a0a52eaec241126839f85b
CyberLynx1021 <abdulazizali234@gmail.com>
2023-07-20 01:42:11 -0500

    React project 1 hr

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
 import './App.css';
-import Todo from './components/Todo.jsx'
-import Titles from './components/Titles.jsx'
-import Modal from './components/Modal.jsx'
-import React, {useState} from 'react'
+import Todo from './components/Todo.jsx';
+import Titles from './components/Titles.jsx';
+import Modal from './components/Modal.jsx';
+import React, {useState} from 'react';
+import React, { useState } from 'react';
+
 function App() {
-useState(false)
- const [showModal] = useState(false)
  
-  return (
-    
-    <>
+  
+ 
+ 
+ 
+ return <Counter />
+ const [showModal, setShowModal] = useState(false)
+
+ 
+ return (
+    <div>
       <Titles />
       <div>
         <input type="text" onChange={(event) => {
           console.log(event.target.value)
         }} />
-        <button>Ad Tod0</button>
+        <button onClick={() => setShowModal(true)}>Ad todo</button>
       </div>
       <div className="todo__wrapper">
         <Todo title ="Finish Frontend Simplfied"/>
         <Todo title ="Finish Interview section"/>
         <Todo title ="Land a 100k Jobs"/>
       </div>
-{  false &&  <Modal title="Confirm Delete?"/> }    
-  </>
+{showModal  &&  <Modal title="Confirm Delete?"/> }    
+  </div>
   );
 }
 
